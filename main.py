d70af74bef46c69554a17a2ab31223356239bcaa
jp-edralin <jpedralin57@gmail.com>
2020-12-01 13:42:02 -0500

    Colored right side

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from PIL import Image
 from typing import Tuple, Dict
 
 import matplotlib.pyplot as plt
+from queue import PriorityQueue, Queue
 
 
 def retrieve_pixels() -> Tuple[np.array, np.array]:
@@ -21,7 +22,7 @@ def retrieve_pixels() -> Tuple[np.array, np.array]:
 
     # Get file path to image
     root_dir = os.getcwd()
-    file_path = os.path.join(root_dir, 'image.jpg')
+    file_path = os.path.join(root_dir, 'flag.jpg')
 
     # Open image
     im = Image.open(file_path)
@@ -201,6 +202,202 @@ def calculate_distance(curr_pixel: np.array, centroid: np.array) -> float:
     return distance
 
 
+def calculate_gray_score(gray: np.array, i: int, j: int) -> int:
+    """
+    Calculate the 'grayness' of a 3x3 grid
+    :param gray: array of gray pixels
+    :param i: x coordinate of pixel
+    :param j: y coordinate of pixel
+    :return: gray score, the sum of the gray values of each pixel in the grid
+    """
+    score = 0
+
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+
+            score += int(gray[i + x][j + y])
+
+            # Prioritize center cell
+            # if x == 0 and y == 0:
+            #     score += 5*int(gray[i + x][j + y])
+            #
+            # else:
+            #     score += int(gray[i + x][j + y])
+
+    return score
+
+def get_similar_gray_patches(gray: np.array, left_gray_scores_dict: Dict[int, List[Tuple[int, int]]], right_i: int, right_j: int) -> List[Tuple[int, Tuple[int, int]]]:
+    """
+    Retrieve the 6 most similar gray patches on the left hand side
+    :param gray: array of gray pixels
+    :param left_gray_scores_dict: gray scores for left hand side
+    :param right_i: x coordinate of right pixel
+    :param right_j: y coordinate of right pixel
+    :return: List of the 6 most similar gray patches
+    """
+    # gray_patch_queue = PriorityQueue()
+    right_gray_score = calculate_gray_score(gray, right_i, right_j)
+    new_score_key_up = right_gray_score
+    new_score_key_down = right_gray_score
+
+    # Retrieve 6 similar patches from dictionary
+    similar_gray_patch_coordinates = left_gray_scores_dict.get(right_gray_score)
+    if similar_gray_patch_coordinates is None:
+        similar_gray_patch_coordinates = []
+    similar_gray_patches = [(0, x) for x in similar_gray_patch_coordinates]
+    # print("similar patches", len(similar_gray_patches), (right_i, right_j))
+
+    # Add more coordinates until you hit 6
+    while len(similar_gray_patches) < 6:
+
+        new_score_key_up += 1
+        new_score_key_down -= 1
+
+        # Go one key up
+        similar_gray_patch_up_coordinates = left_gray_scores_dict.get(new_score_key_up)
+        if similar_gray_patch_up_coordinates is None:
+            similar_gray_patch_up_coordinates = []
+        similar_gray_patches_up = [(new_score_key_up-right_gray_score, x) for x in similar_gray_patch_up_coordinates]
+        similar_gray_patches.extend(similar_gray_patches_up)
+
+        # Go one key down
+        similar_gray_patch_down_coordinates = left_gray_scores_dict.get(new_score_key_down)
+        if similar_gray_patch_down_coordinates is None:
+            similar_gray_patch_down_coordinates = []
+        similar_gray_patches_down = [(right_gray_score - new_score_key_down, x) for x in similar_gray_patch_down_coordinates]
+        similar_gray_patches.extend(similar_gray_patches_down)
+        # print("papa")
+
+    return similar_gray_patches[:6]
+
+
+    # Iterate through left half of gray, skipping edges
+    # for i in range(1, gray.shape[0]-1):
+    #     for j in range(1, int(gray.shape[1]/2) - 1):
+    #         left_gray_score = calculate_gray_score(gray, i, j)
+    #         score_difference = abs(right_gray_score-left_gray_score)
+    #
+    #         # Add gray patch to priority queue if there's space
+    #         if gray_patch_queue.qsize() < 6:
+    #             gray_patch_queue.put((-1*score_difference, (i, j)))
+    #
+    #         # Check to see if current gray patch is more similar; if so add to queue
+    #         else:
+    #             lowest_similarity_patch = gray_patch_queue.get()
+    #
+    #             # Current patch is more similar
+    #             if -1*lowest_similarity_patch[0] > score_difference:
+    #                 gray_patch_queue.put((-1*score_difference, (i, j)))
+    #
+    #             # If not, put the lowest similarity patch back
+    #             else:
+    #                 gray_patch_queue.put(lowest_similarity_patch)
+
+    # return list(gray_patch_queue.queue)
+
+
+def get_left_gray_scores(gray: np.array) -> Dict[int, List[Tuple[int, int]]]:
+    """
+    Get a list of all the gray scores for the left side of the image
+    :param gray: array of gray pixels
+    :return: Dictionary with gray score as the key and coordinates as the value
+    """
+    left_gray_scores_dict = {}
+
+    # Iterate through left half of gray, skipping edges
+    for i in range(1, gray.shape[0]-1):
+        for j in range(1, int(gray.shape[1]/2) - 1):
+            left_gray_score = calculate_gray_score(gray, i, j)
+
+            if left_gray_score in left_gray_scores_dict:
+                curr_list = left_gray_scores_dict.get(left_gray_score)
+                curr_list.append((i, j))
+                left_gray_scores_dict.update({left_gray_score: curr_list})
+            else:
+                left_gray_scores_dict[left_gray_score] = [(i, j)]
+
+    return left_gray_scores_dict
+
+
+
+def color_right_side(gray: np.array, new_rgb: np.array, representative_colors: np.array, pixel_color_array: np.array) -> np.array:
+    """
+    Color the right side of the grayscale image
+    :param gray: array of gray pixels
+    :param new_rgb: array that will be colored
+    :param representative_colors: 5 colors that will color the image
+    :param pixel_color_array: the current colors for each pixel
+    :return: a fully colored rgb image
+    """
+    print("starting right side coloring")
+
+    num_rows = new_rgb.shape[0]
+    num_cols = new_rgb.shape[1]
+
+    # similar_gray_patch_queue = None
+
+    left_gray_scores_dict = get_left_gray_scores(gray)
+    print(left_gray_scores_dict)
+
+    # Fill in right half of new_rgb with new colors, ignoring edges
+    for i in range(1, num_rows-1):
+        for j in range(int(num_cols / 2), num_cols-1):
+
+            # Retrieve most 6 most similar patch centers and their similarity scores
+            similar_gray_patches = get_similar_gray_patches(gray, left_gray_scores_dict, i, j)
+            # print("retrieving 6 patches")
+            # print(similar_gray_patches)
+
+            # Find representative color for each patch and add patch to
+            patches_for_each_color: List[List[Tuple[int, Tuple[int, int]]]] = [[] for _ in range(5)]
+            max_color_frequency = 0
+            for patch in similar_gray_patches:
+                color_index = int(pixel_color_array[(patch[1][0] * num_cols + patch[1][1])][3])
+                # color_index = new_rgb[patch[1][0]][patch[1][1]][3]
+                # color_counts[color_index] += 1
+
+                patches_for_each_color[color_index].append(patch)
+
+                if len(patches_for_each_color[color_index]) > max_color_frequency:
+                    max_color_frequency = len(patches_for_each_color[color_index])
+
+            # print("patches for each color", patches_for_each_color, (i, j))
+            # Retrieve all color indices with that max frequency
+            most_frequent_color_indices = []
+            for x in range(len(patches_for_each_color)):
+                if len(patches_for_each_color[x]) == max_color_frequency:
+                    most_frequent_color_indices.append(x)
+
+            # If there is a most represented color, make the corresponding pixel that color
+            if len(most_frequent_color_indices) == 1:
+                new_rgb[i][j] = representative_colors[most_frequent_color_indices[0]]
+                # print("no tie", (i, j), representative_colors[most_frequent_color_indices[0]])
+
+            # Otherwise, break ties based on similarity score
+            else:
+                # print("there is a tie", (i, j))
+                potential_patches = []
+
+                # Put all patches that map to the most represented colors into potential_patches
+                for x in range(len(patches_for_each_color)):
+                    if len(patches_for_each_color[x]) == max_color_frequency:
+                        for patch in patches_for_each_color[x]:
+                            potential_patches.append(patch)
+
+                # Select the color that is mapped to the most similar patch
+                best_similarity_score = 100000
+                most_similar_patch = None
+                for patch in potential_patches:
+                    if patch[0] < best_similarity_score:
+                        best_similarity_score = patch[0]
+                        most_similar_patch = patch
+
+                # Make the original pixel the same color as that of the most similar patch
+                new_rgb[i][j] = new_rgb[most_similar_patch[1][0]][most_similar_patch[1][1]]
+
+    return new_rgb
+
+
 def main():
     print('hello world')
     rgb, gray = retrieve_pixels()
@@ -215,7 +412,7 @@ def main():
     for i in range(num_rows):
         for j in range(0, int(num_cols/2)):
             color_index = int(pixel_color_array[(i * num_cols + j)][3])
-            print(color_index)
+            # print(color_index)
             new_rgb[i][j] = representative_colors[color_index]
 
     np.set_printoptions(threshold=5)
@@ -224,9 +421,14 @@ def main():
     plt.imshow(new_rgb.astype('uint8'))
     plt.show()
 
+    # Color right hand side
+    newest_rgb = color_right_side(gray, new_rgb, representative_colors, pixel_color_array)
+    plt.imshow(newest_rgb.astype('uint8'))
+    plt.show()
+
     # print(gray)
     # print(gray.shape)
-    # plt.imshow(gray)
+    # plt.imshow(gray, cmap='gray')
     # plt.show()
 
     print('goodbye world')
